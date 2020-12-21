9731341b014d993283dbee620ecba53ca643bf8c
Aaron Cardwell <aaron_cardwell@hotmail.com>
2020-12-21 23:48:36 +0000

    Added main class
    - To provide clear start point
    - Removes FXML code from user class (cleaner)

---
+++ TableTennisOrganiser/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("User.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- TableTennisOrganiser/src/main/User.java
+++ TableTennisOrganiser/src/main/User.java
@@ -1,24 +1,7 @@
 package main;
-	
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
-public class User extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("User.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Login");
-        stage.show();
-    }
-	
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class User   {
+    
     private String userId;
 
     private String password;
