4496ccb071c314bb1012e26a2e6cbc5c1869cd65
Aaron Cardwell <aaron_cardwell@hotmail.com>
2020-12-27 14:14:25 +0000

    Separated from Viewer Controller, now inherits from UserController
    Enables better separation of viewer and admin UIs on initialisation of scenes

--- TableTennisOrganiser/src/main/AdminController.java
+++ TableTennisOrganiser/src/main/AdminController.java
@@ -1,19 +1,55 @@
 package main;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 
 /**
  * Controls FMXL GUI elements for Admin class
  * @author Aaron
  */
-public class AdminController extends ViewerController implements Initializable{
-       
+public class AdminController extends UserController implements Initializable{
+    
+    @FXML private ListView<String> teamView;
+    @FXML private ChoiceBox<String> leagueChoiceBox;
+    /**
+     * initialises the UI elements and associated class data structures with
+     * data from the database files.
+     * @param location
+     * @param resources 
+     */
+
     @Override
-    public void start() throws IOException
-    {
-        //Create new viewer class at logon
-        Admin admin = new Admin();
-        admin.startAdmin();
+    public void initialize(URL location, ResourceBundle resources) {
+        Admin admin = new Admin();   //Create object of class to control
+        //Load lsit view with team names.
+        try {
+            admin.startAdmin();
+            teamView.getItems().addAll(admin.viewTeams());
+            //Add listener for Teams view selections
+            teamView.getSelectionModel().selectedItemProperty().addListener(
+                    (ObservableValue<? extends String> observable, 
+                            String oldValue, String newValue) -> {
+                System.out.println("Selected item: " + newValue);
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(AdminController.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+        //Populate league choicebox with league names.
+        ArrayList<String> leagueNames = admin.viewLeagues();
+        for (String choices : leagueNames)  {
+            leagueChoiceBox.getItems().add(choices);
+            leagueChoiceBox.setValue(choices);
+        }
     }
+
 }
