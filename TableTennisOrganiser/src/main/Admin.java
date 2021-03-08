a853576a4d5fac6f6a11e2bf4f4114ba154c90fd
ae2-cardwell <aaron_cardwell@hotmail.com>
2021-03-08 13:22:55 +0000

    Code cleanup and JavaDoc commenting.

--- TableTennisOrganiser/src/main/Admin.java
+++ TableTennisOrganiser/src/main/Admin.java
@@ -10,9 +10,10 @@ import java.util.ArrayList;
 
 /**
  * <h1>Admin Logic Class</h1>
- * Class which implements all logic for Admin functions and use cases.
+ * Class which implements all logic for <code>Admin</code> functions and use 
+ * cases.
  * @author  Aaron Cardwell 13009941
- * @version 0.1
+ * @version 1.0
  * @since 06/12/2020
  */
 public class Admin extends Timer {
@@ -22,11 +23,10 @@ public class Admin extends Timer {
      * @param super.fileName
      */
     void saveLeagues() {
-        //System.out.println("Saving leagues to file " + super.getFileName());
         try (FileOutputStream fos = new FileOutputStream(super.getFileName()); 
                 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
                     oos.writeObject(getLeagues());
-            //System.out.println("Leagues Saved to: " + super.getFileName());
+            System.out.println("Leagues Saved to: " + super.getFileName());
         } catch (FileNotFoundException ex) {
             System.err.println(super.getFileName() + " not found");
         } catch (IOException ex) {
@@ -35,17 +35,16 @@ public class Admin extends Timer {
     } 
     /**
      * Loads ALL leagues from a serialized .bin file. Gets file name from 
-     * super class 'User'.
+     * super class <code>User</code> getFileName().
      * @param super.fileName
      * @throws ClassNotFoundException (there will always be a class here)
      */
     void loadLeagues() throws ClassNotFoundException {
-        //System.out.println("Loading leagues from file " + super.getFileName());
         try {
             FileInputStream fis = new FileInputStream(super.getFileName());
             ObjectInputStream ois = new ObjectInputStream(fis);
             setLeagues((ArrayList<League>) ois.readObject());
-            //System.out.println("Leagues loaded from: " + super.getFileName());
+            System.out.println("Leagues loaded from: " + super.getFileName());
         } catch (FileNotFoundException ex) {
             System.err.println("ERROR: File " + super.getFileName() + 
                     " not found");               
@@ -54,45 +53,50 @@ public class Admin extends Timer {
         }
     }
     /**
-     * Adds a 'Team' object to the 'League' object which is passed to the 
-     * function, assigns the new team the 'name' passed to function.
+     * Adds a <code>Team</code> object to the <code>League</code> object which 
+     * is passed to the function, assigns the new <code>Team</code> the 
+     * <code>name</code> passed to function.
      * @param league
      * @param name 
      */
     public void addTeam(League league, String name) {
-        //System.out.println("Adding Team " + name + " to League " + league.getName());
+        System.out.println("Adding Team " + name + " to League " + league.getName());
         league.addTeam(name);
     }
     /**
-     * Removes a 'Team' object from the 'League' object passed to the function.
+     * Removes a <code>Team</code> object from the <code>League</code> object 
+     * passed to the function.
      * @param league
      * @param team 
      */
     public void removeTeam(League league, Team team) {
-        //System.out.println("Removing team "  + team.getName() + " from league "+ league.getName());
+        System.out.println("Removing team "  + team.getName() + " from league "+ league.getName());
         league.removeTeam(team);
     }
     /**
-     * Adds a 'Player' object to the 'Team' object which is passed to the 
-     * function, assigns the new player the 'name' passed to function.
+     * Adds a <code>Player</code> object to the <code>Team</code> object which is passed to the 
+     * function, assigns the new <code>Player</code> the <code>name</code> 
+     * passed to function.
      * @param team
      * @param name
      */
     public void addPlayer(Team team, String name) {
-        //System.out.println("Adding Player " + name + " to Team " + team.getName());
+        System.out.println("Adding Player " + name + " to Team " + team.getName());
         team.addPlayer(new Player(name));
     }
     /**
-     * Removes a 'Player' object from the 'Team' object passed to the function.
+     * Removes a <code>Player</code> object from the <code>Team</code> object 
+     * passed to the function.
      * @param team 
      * @param player
      */
     public void removePlayer(Team team, Player player) {
-        //System.out.println("Removing player "  + player.getName() + " from team "+ team.getName());
+        System.out.println("Removing player "  + player.getName() + " from team "+ team.getName());
         team.removePlayer(player);
     }
     /**
-     * Updates the 'Team' objects 'venue' string with the name passed.
+     * Updates the <code>Team</code> objects <code>venue</code> string with the 
+     * <code>name</code> passed.
      * @param team
      * @param name 
      */
@@ -100,12 +104,15 @@ public class Admin extends Timer {
         team.setVenue(name);
     }
     /**
-     * Generates and REPLACES all fixtures for the league passed.
+     * Generates and REPLACES all <code>fixtures</code> for the 
+     * <code>League</code> passed. If <code>homeAndAway</code> is set to true, 
+     * generates <code>fixtures</code> where each <code>Team</code> plays each 
+     * other <code>Team</code> in BOTH a home AND away <code>Fixture</code>.
      * @param league 
      */
     void generateFixtures(League league, boolean homeAndAway, String venue) {
         league.countTeams();
-        //System.out.println("Generating fixtures for " + league.getTeamsCount() + " teams.");
+        System.out.println("Generating fixtures for " + league.getTeamsCount() + " teams.");
         //How many teamsCount are in the league?
         int teamsCount = league.getTeamsCount();
         int increment;
@@ -146,52 +153,43 @@ public class Admin extends Timer {
         if (bye == true) {
             league.getTeams().remove(teamsCount - 1);
         }
-        //update League object with new fixtures
+        //Update League object with new fixtures
         league.appendFixtures(fixtures);
         league.countFixtures();
+        //Recursively run again if home and away fixtures are requested.
         if(homeAndAway) {
             generateFixtures(league, false, "away");
         }
     }
-
-    private static String flip(String match) {
-        String[] components = match.split(" v ");
-        return components[1] + " v " + components[0];
-    }
     
     /**
-     * Updates the score sheet for the given fixture. Scores must be passed as
-     * an array of strings in the following format:
+     * Updates the score sheet for the given <code>Fixture</code>. 
+     * <code>scores</code> must be passed as an array of strings in the 
+     * following format:
      * (int)home score : (int)away score
-     * 4x3 singles games
-     * 1x3 doubles games
+     * 4x3 sets of singles games
+     * 1x3 sets doubles games
      * @param fixture 
      */
     void modifyScoreSheet(Fixture fixture, ArrayList<String> scores) {
         fixture.setScores(scores);
     }
-
-    void saveLeagueToDatabase(League league) {
-
-    }
-
-    void registerPlayer(String name, Team team, League league) {
-
-    }
+    
     /**
-     * Force updates ALL fixturesCount int parameters in ALL 'Team' objects 
-     * currently assigned to the Admin user. For use after adding or removing
-     * fixtures, to update Teams TableView fixtures column.
-     * **WILL BE REMOVED FOR EFFICIENCY REASONS EVENTUALLY**
+     * Force updates ALL <code>fixturesCount</code> int parameters in ALL 
+     * <code>Team</code> objects currently assigned to the <code>Admin</code> 
+     * class. For use after adding or removing <code>fixtures</code>, to update 
+     * teamsTableView fixtures column.
      */
     void countFixtures()   {
-        getLeagues().forEach(temp -> {
-            temp.countFixtures();
+        getLeagues().forEach(league -> {
+            league.countFixtures();
         });
     }
     /**
-     * Adds a new blank fixture to the passed league.
+     * Adds a new blank <code>fixture</code> to the passed <code>league</code>.
      * @param league
+     * @return Fixture
      */
     Fixture createFixture(League league) {
         Team team = new Team("None");
@@ -199,25 +197,31 @@ public class Admin extends Timer {
         league.addFixture(fixture);
         return fixture;
     }
-
+    /**
+     * Removes all <code>fixtures</code> from <code>League</code>.
+     * @param league 
+     */
     void deleteFixtures(League league) {
         league.resetFixtures();
         league.countFixtures();
     }
-    
+    /**
+     * Removes a single <code>Fixture</code> from a <code>League</code>
+     * @param league
+     * @param fixture 
+     */
     void removeFixture(League league, Fixture fixture) {
         league.removeFixture(fixture);
         league.countFixtures();
     }
 
-    void updateTeams() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
     void setFixtureVenue(Fixture fixture, String venue) {
         fixture.setVenue(venue);
     }
-    
+    /**
+     * Returns names of all <code>Leagues</code> in database.
+     * @return 
+     */
     protected ArrayList<String> viewLeagues() {
         ArrayList<String> leagueNames = new ArrayList<>();
         ArrayList<League> leagues = getLeagues();
